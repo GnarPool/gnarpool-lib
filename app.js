34a04f6b9f8362f93540b4df1012dc4cfd650426
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-04-26 10:08:49 -0600

    set up initial directory structure

---
+++ app.js
@@ -0,0 +1,26 @@
+﻿// Application initialization and entry point
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var db = require('./db');
+var routes = require('./controllers/index');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+// setup body parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// pass requests to routers
+app.use('/', routes);
+
+// connect to database and listen for client connections
+db.connect(function (error) {
+	if (error) throw error;
+	app.listen(port);
+});
---
+++ db.js
@@ -0,0 +1,9 @@
+﻿var mongoose = require('mongoose');
+var mongoURL = "mongodb://gnarpool:shr3dTheGnar@ds021671.mlab.com:21671/gnarpool-db";
+
+exports.connect = function (callback) {
+	mongoose.connect(mongoURL);
+	var db = mongoose.connection;
+	db.on('error', function (error) { callback(error) });
+	db.on('open', function () { callback() });
+}
