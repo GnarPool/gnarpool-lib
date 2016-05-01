e085ed0435e34d59ff13fb43e1816b657c6d60ff
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-05-01 08:47:38 -0600

    added user and trip models with skeleton functionality

---
+++ models/trip.js
@@ -0,0 +1,15 @@
+﻿var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// require the user schema for embedding
+var userSchema = require('./user.js').schema;
+
+var tripModel = new Schema({
+	driver: userSchema,
+	passengers: [userSchema],
+	origin: String, // what about the case where passengers get on at different locations?
+	destination: String
+});
+
+// expose the model to the app
+module.exports = mongoose.model('Trip', tripModel);
---
+++ models/user.js
@@ -0,0 +1,12 @@
+﻿var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+	fname: String,
+	lname: String,
+	email: String,
+	password: String
+});
+
+// expose the model to the app
+module.exports = mongoose.model('User', userSchema);
